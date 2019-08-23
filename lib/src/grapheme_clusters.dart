5c328e16050754fe39e8626de114a5a5eec7c202
Lasse R.H. Nielsen <lrn@google.com>
2019-08-23 11:03:20 +0200

    Add more features.
    Restructure support functionality to support backwards iteration.
    Make `GraphemeCluster` iterator bidirectional.
    Add more methods to `GraphemeClusters`.
    Add test.

--- lib/src/grapheme_clusters.dart
+++ lib/src/grapheme_clusters.dart
@@ -19,7 +19,7 @@ Iterable<int> graphemeClusterBoundaries(String string,
     [int start = 0, int end]) sync* {
   end = RangeError.checkValidRange(start, end, string.length);
   var breaks = Breaks(string, start, end, stateSoT);
-  var breakAt;
+  int breakAt;
   while ((breakAt = breaks.nextBreak()) >= 0) yield breakAt;
 }
 
@@ -38,6 +38,19 @@ abstract class GraphemeClusters implements Iterable<String> {
   // as well as controlling the iteration in more detail.
   GraphemeCluster get iterator;
 
+  /// Whether [graphemeCluster] is an element of this sequence of
+  /// grapheme clusters.
+  ///
+  /// Returns false if [graphemeCluster] is not a string containing
+  /// a single grapheme cluster,
+  /// because then it is not a single element of this [Iterable]
+  /// of grapheme clusters.
+  bool contains(Object graphemeCluster);
+
+  /// Whether this sequence of grapheme clusters contains [other]
+  /// as a subsequence.
+  bool containsAll(GraphemeClusters other);
+
   /// The first string index where [other] occurs as a subsequence of these
   /// grapheme clusters.
   ///
@@ -53,8 +66,16 @@ abstract class GraphemeClusters implements Iterable<String> {
   /// of the substring of [string] starting at [startIndex].
   bool startsWith(GraphemeClusters other, [int startIndex = 0]);
 
+  /// Whether [other] is an trailing subsequence of this sequence
+  /// of grapheme clusters.
+  ///
+  /// If [endIndex] is provided, then checks whether
+  /// [other] is an trailing subsequence of the grapheme clusters
+  /// of the substring of [string] ending at [endIndex].
+  bool endsWith(GraphemeClusters other, [int endIndex]);
+
   /// Returns a new grapheme clusters sequence where [source] has been
-  /// replaced by [replacement].
+  /// replaced by [replacement].start
   ///
   /// If [startIndex] is provided, instead replaces grapheme clusters of the
   /// substring of [string] starting at [startIndex].
@@ -79,7 +100,7 @@ abstract class GraphemeClusters implements Iterable<String> {
 }
 
 /// Iterator over grapheme clusters of a string.
-abstract class GraphemeCluster implements Iterator<String> {
+abstract class GraphemeCluster implements BidirectionalIterator<String> {
   /// Creates a new grapheme cluster iterator iterating the grapheme
   /// clusters of [string].
   factory GraphemeCluster(String string) = _GraphemeCluster;
@@ -107,13 +128,14 @@ abstract class GraphemeCluster implements Iterator<String> {
   /// The code units of the current grapheme cluster.
   List<int> get codeUnits;
 
-  /// The code poin;ts of the current grapheme cluster.
+  /// The code points of the current grapheme cluster.
   Runes get runes;
 
   /// Resets the iterator to the [index] position.
   ///
   /// There is no [current] grapheme cluster after a reset,
-  /// a call to [moveNext] is needed.
+  /// a call to [moveNext] is needed to find the end of the grapheme cluster
+  /// at the [index] position.
   /// A `reset(0)` will reset to the beginning of the string, as for a newly
   /// created iterator.
   void reset(int index);
@@ -121,6 +143,10 @@ abstract class GraphemeCluster implements Iterator<String> {
   /// Creates a copy of this [GraphemeIterator].
   ///
   /// The copy is in the exact same state as this iterator.
+  /// Can be used to iterate the following grapheme clusters more than once
+  /// at the same time. To simply rewind an iterator, remember the
+  /// [start] or [end] position and use [reset] to reset the iterator
+  /// to that position.
   GraphemeCluster copy();
 }
 
@@ -132,61 +158,167 @@ class _GraphemeClusters extends Iterable<String> implements GraphemeClusters {
 
   GraphemeCluster get iterator => _GraphemeCluster(string);
 
-  int indexOf(GraphemeClusters other, [int startIndex = 0]) {
-    RangeError.checkValueInInterval(startIndex, 0, null, "startIndex");
-    var startBreaks =
-        Breaks(string, startIndex, string.length, stateSoTNoBreak);
-    var endBreaks = Breaks(string, startIndex, string.length, stateSoTNoBreak);
-    int start = startIndex;
-    int end = startIndex;
-    int otherLength = other.string.length;
-    do {
-      int length = end - start;
-      if (length == otherLength && string.startsWith(other.string, start)) {
-        return start;
-      } else if (length < otherLength) {
-        end = endBreaks.nextBreak();
-      } else {
-        assert(length > otherLength);
-        start = startBreaks.nextBreak();
+  String get first => string.isEmpty
+      ? throw StateError("No element")
+      : string.substring(
+          0, Breaks(string, 0, string.length, stateSoTNoBreak).nextBreak());
+
+  String get last => string.isEmpty
+      ? throw StateError("No element")
+      : string.substring(
+          BackBreaks(string, string.length, 0, stateEoTNoBreak).nextBreak());
+
+  String get single {
+    if (string.isEmpty) throw StateError("No element");
+    int firstEnd =
+        Breaks(string, 0, string.length, stateSoTNoBreak).nextBreak();
+    if (firstEnd == string.length) return string;
+    throw StateError("Too many elements");
+  }
+
+  bool get isEmpty => string.isEmpty;
+
+  bool get isNotEmpty => string.isNotEmpty;
+
+  int get length {
+    if (string.isEmpty) return 0;
+    var brk = Breaks(string, 0, string.length, stateSoTNoBreak);
+    int length = 0;
+    while (brk.nextBreak() >= 0) length++;
+    return length;
+  }
+
+  Iterable<T> whereType<T>() {
+    Iterable<Object> self = this;
+    if (self is Iterable<T>) {
+      return self.map((x) => x);
+    }
+    return Iterable<T>.empty();
+  }
+
+  String join([String separator = ""]) {
+    if (separator == "") return string;
+    return super.join(separator);
+  }
+
+  String lastWhere(bool test(String element), {String orElse()}) {
+    int cursor = string.length;
+    var brk = BackBreaks(string, cursor, 0, stateEoTNoBreak);
+    int next = 0;
+    while ((next = brk.nextBreak()) >= 0) {
+      String current = string.substring(next, cursor);
+      if (test(current)) return current;
+      cursor = next;
+    }
+    if (orElse != null) return orElse();
+    throw StateError("no element");
+  }
+
+  bool contains(Object other) {
+    if (other is String) {
+      if (other.isEmpty) return false;
+      int i = 0;
+      while (i + other.length <= string.length) {
+        int index = string.indexOf(other, i);
+        if (index < 0) return false;
+        if (isGraphemeClusterBoundary(string, 0, string.length, index)) {
+          int next =
+              Breaks(string, index, string.length, stateSoTNoBreak).nextBreak();
+          assert(next >= 0);
+          if (next == index + other.length) return true;
+        }
+        i = index + 1;
       }
-    } while (end >= 0);
+    }
+    return false;
+  }
+
+  int indexOf(GraphemeClusters other, [int startIndex = 0]) {
+    int length = string.length;
+    RangeError.checkValidRange(startIndex, length, length, "startIndex");
+    return _indexOf(other, startIndex);
+  }
+
+  int _indexOf(GraphemeClusters other, int startIndex) {
+    String otherString = other.string;
+    int otherLength = otherString.length;
+    if (otherLength == 0) return startIndex;
+    int length = string.length;
+    int index = startIndex;
+    while (index + otherLength <= length) {
+      int matchIndex = string.indexOf(otherString, startIndex);
+      if (matchIndex < 0) return matchIndex;
+      if (isGraphemeClusterBoundary(string, startIndex, length, matchIndex) &&
+          isGraphemeClusterBoundary(
+              string, startIndex, length, matchIndex + otherLength)) {
+        return matchIndex;
+      }
+      startIndex = matchIndex + 1;
+    }
     return -1;
   }
 
   bool startsWith(GraphemeClusters other, [int startIndex = 0]) {
-    RangeError.checkValueInInterval(startIndex, 0, null, "startIndex");
-    if (other.string.isEmpty) return true;
-    if (!string.startsWith(other.string, startIndex)) return false;
-    int end = startIndex + other.string.length;
-    var breaks = Breaks(string, startIndex, string.length, stateSoTNoBreak);
-    int nextBreak = 0;
-    do {
-      nextBreak = breaks.nextBreak();
-      if (nextBreak == end) return true;
-    } while (nextBreak < end);
-    return false;
+    RangeError.checkValueInInterval(startIndex, 0, string.length, "startIndex");
+    String otherString = other.string;
+    if (otherString.isEmpty) return true;
+    return string.startsWith(otherString, startIndex) &&
+        isGraphemeClusterBoundary(
+            string, 0, string.length, startIndex + otherString.length);
+  }
+
+  bool endsWith(GraphemeClusters other, [int endIndex]) {
+    String otherString = other.string;
+    if (otherString.isEmpty) return true;
+    int length = string.length;
+    int otherLength = otherString.length;
+    if (endIndex == null || endIndex == length) {
+      return string.endsWith(otherString) &&
+          isGraphemeClusterBoundary(string, 0, length, length - otherLength);
+    }
+    int start = endIndex - otherLength;
+    return start >= 0 &&
+        string.startsWith(otherString, start) &&
+        isGraphemeClusterBoundary(string, 0, endIndex, start);
   }
 
   GraphemeClusters replaceAll(
-      GraphemeClusters source, GraphemeClusters replacement,
+      GraphemeClusters pattern, GraphemeClusters replacement,
       [int startIndex = 0]) {
-    if (startIndex != 0) {
+    if (startIndex > 0) {
       RangeError.checkValueInInterval(
           startIndex, 0, string.length, "startIndex");
+    }
+    if (pattern.string.isEmpty) {
+      if (string.isEmpty) return replacement;
+      return GraphemeClusters(_explodeReplace(replacement.string, startIndex));
     }
     int start = startIndex;
     StringBuffer buffer;
     int next = -1;
-    while ((next = this.indexOf(source, start)) >= 0) {
+    while ((next = this.indexOf(pattern, start)) >= 0) {
       (buffer ??= StringBuffer())
         ..write(string.substring(start, next))
         ..write(replacement);
-      start = next + source.string.length;
+      start = next + pattern.string.length;
     }
     if (buffer == null) return this;
     buffer.write(string.substring(start));
     return GraphemeClusters(buffer.toString());
+  }
+
+  // Replaces every grapheme cluster boundary with [replacement].
+  // Starts at startIndex.
+  String _explodeReplace(String replacement, int startIndex) {
+    var buffer = StringBuffer(string.substring(0, startIndex));
+    var breaks = Breaks(string, startIndex, string.length, stateSoTNoBreak);
+    int index = 0;
+    while ((index = breaks.nextBreak()) >= 0) {
+      buffer..write(replacement)..write(string.substring(startIndex, index));
+      startIndex = index;
+    }
+    buffer.write(replacement);
+    return buffer.toString();
   }
 
   GraphemeClusters replaceFirst(
@@ -201,17 +333,29 @@ class _GraphemeClusters extends Iterable<String> implements GraphemeClusters {
     return GraphemeClusters(string.replaceRange(
         index, index + source.string.length, replacement.string));
   }
+
+  bool containsAll(GraphemeClusters other) {
+    return _indexOf(other, 0) >= 0;
+  }
 }
 
 class _GraphemeCluster implements GraphemeCluster {
+  static const int _directionForward = 0;
+  static const int _directionBackward = 0x04;
+  static const int _directionMask = 0x04;
+  static const int _cursorDeltaMask = 0x03;
+
   final String _string;
   int _start;
   int _end;
-  Breaks _breaks;
+  // Encodes current state,
+  // whether we are moving forwards or backwards ([_directionMask]),
+  // and how far ahead the cursor is from the start/end ([_cursorDeltaMask]).
+  int _state;
   String _currentCache;
 
-  _GraphemeCluster(String string) : this._(string, 0, 0, null);
-  _GraphemeCluster._(this._string, this._start, this._end, this._breaks);
+  _GraphemeCluster(String string) : this._(string, 0, 0, stateSoTNoBreak);
+  _GraphemeCluster._(this._string, this._start, this._end, this._state);
 
   int get start => _start;
   int get end => _end;
@@ -220,39 +364,65 @@ class _GraphemeCluster implements GraphemeCluster {
       (_start == _end ? null : _string.substring(_start, _end));
 
   bool moveNext() {
-    int next = (_breaks ??=
-            Breaks(_string, _start, _string.length, stateSoTNoBreak))
-        .nextBreak();
+    int state = _state;
+    int cursor = _end;
+    if (state & _directionMask != _directionForward) {
+      state = stateSoTNoBreak;
+    } else {
+      cursor += state & _cursorDeltaMask;
+    }
+    var breaks = Breaks(_string, cursor, _string.length, state);
+    var next = breaks.nextBreak();
     _currentCache = null;
     if (next >= 0) {
       _start = _end;
       _end = next;
+      _state =
+          (breaks.state & 0xF0) | _directionForward | (breaks.cursor - next);
       return true;
     }
+    _state = stateEoTNoBreak | _directionBackward;
     _start = _end;
+    return false;
+  }
+
+  bool movePrevious() {
+    int state = _state;
+    int cursor = _start;
+    if (state & _directionMask == _directionForward) {
+      state = stateEoTNoBreak;
+    } else {
+      cursor -= state & _cursorDeltaMask;
+    }
+    var breaks = BackBreaks(_string, cursor, 0, state);
+    var next = breaks.nextBreak();
+    _currentCache = null;
+    if (next >= 0) {
+      _end = _start;
+      _start = next;
+      _state =
+          (breaks.state & 0xF0) | _directionBackward | (next - breaks.cursor);
+      return true;
+    }
+    _state = stateSoTNoBreak | _directionForward;
+    _end = start;
     return false;
   }
 
   List<int> get codeUnits => _CodeUnits(_string, _start, _end);
 
-  // TODO: Make [Runes] constructor accept a start and end, then use that.
   Runes get runes => Runes(current);
 
   void reset(int index) {
     RangeError.checkValueInInterval(index, 0, _string.length, "index");
-    if (index != _end) _breaks = null;
+    _state = stateSoTNoBreak | _directionForward;
     _currentCache = null;
     _start = _end = index;
   }
 
   GraphemeCluster copy() {
-    return _GraphemeCluster._(_string, _start, _end, _breaks.copy());
+    return _GraphemeCluster._(_string, _start, _end, _state);
   }
-
-  // TODO: Optimize these to not create the substrings.
-  int get hashCode => current.hashCode;
-  bool operator ==(Object other) =>
-      other is GraphemeCluster && current == other.current;
 }
 
 class _CodeUnits extends ListBase<int> {
@@ -263,8 +433,9 @@ class _CodeUnits extends ListBase<int> {
   _CodeUnits(this._string, this._start, this._end);
 
   int get length => _end - _start;
+
   int operator [](int index) {
-    RangeError.checkValidIndex(index, this);
+    RangeError.checkValidIndex(index, this, "index", _end - _start);
     return _string.codeUnitAt(_start + index);
   }
 
--- lib/src/grapheme_clusters/breaks.dart
+++ lib/src/grapheme_clusters/breaks.dart
@@ -1,20 +1,48 @@
-// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 import "constants.dart";
 import "table.dart";
 
+/// Iterates grapheme cluster breaks of a string.
+///
+/// Iterates the grapheme cluster breaks of the substring of
+/// [base] from [cursor] to [end].
+///
+/// To iterate a substring, use:
+/// ```dart
+/// var breaks = Breaks(string, start, end, stateSoT);
+/// int brk = 0;
+/// while((brk = breaks.nextBreak) >= 0) {
+///   print("Break at index $brk");
+/// }
+/// ```
+/// If you use [stateSoTNoBreak] instead of [stateSoT], the
+/// initial break between the start-of-text and the first grapheme
+/// is suppressed.
 class Breaks {
+  /// Text being iterated.
   final String base;
+
+  /// end of substring of [base] being iterated.
   final int end;
+
+  /// Position of the first yet-unprocessed code point.
   int cursor;
+
+  /// Current state based on code points processed so far.
   int state;
 
   Breaks(this.base, this.cursor, this.end, this.state);
 
+  /// Creates a copy of the current iteration, at the exact same state.
   Breaks copy() => Breaks(base, cursor, end, state);
 
+  /// The index of the next grapheme cluster break in last-to-first index order.
+  ///
+  /// Returns a negative number if there are no further breaks,
+  /// which means that [cursor] has reached [end].
   int nextBreak() {
     while (cursor < end) {
       int breakAt = cursor;
@@ -44,4 +72,222 @@ class Breaks {
     if (state & stateNoBreak == 0) return cursor;
     return -1;
   }
+}
+
+/// Iterates grapheme cluster breaks backwards.
+///
+/// Given a substring of a [base] string from [start] to [cursor],
+/// iterates the grapheme cluster breaks from [cursor] to [start].
+///
+/// To iterate a substring, do
+/// ```dart
+/// var breaks = BackBreaks(string, start, end, stateEoT);
+/// int brk = 0;
+/// while ((brk = breaks.nextBreak()) >= 0) {
+///   print("Break at index $brk");
+/// }
+/// ```
+/// If the initial [state] is [stateEoTNoBreak] instead of [stateEoT],
+/// the initial break between the last grapheme and the end-of-text
+/// is suppressed.
+class BackBreaks {
+  /// Text being iterated.
+  final String base;
+
+  /// Start of substring of [base] being iterated.
+  final int start;
+
+  /// Position after the last yet-unprocessed code point.
+  int cursor;
+
+  /// Current state based on code points processed so far.
+  int state;
+  BackBreaks(this.base, this.cursor, this.start, this.state);
+
+  BackBreaks copy() => BackBreaks(base, cursor, start, state);
+
+  /// The index of the next grapheme cluster break in first-to-last index order.
+  ///
+  /// Returns a negative number if there are no further breaks,
+  /// which means that [cursor] has reached [start].
+  int nextBreak() {
+    while (cursor > start) {
+      int breakAt = cursor;
+      int char = base.codeUnitAt(--cursor);
+      if (char & 0xFC00 != 0xDC00) {
+        state = moveBack(state, low(char));
+        if (state >= stateLookaheadMin) state = _lookAhead(state);
+        if (state & stateNoBreak == 0) {
+          return breakAt;
+        }
+        continue;
+      }
+      // The category of an unpaired tail surrogate is Control.
+      int category = categoryControl;
+      if (cursor >= start) {
+        int prevChar = base.codeUnitAt(cursor - 1);
+        if (prevChar & 0xFC00 == 0xD800) {
+          category = high(prevChar, char);
+          cursor -= 1;
+        }
+      }
+      state = moveBack(state, category);
+      if (state >= stateLookaheadMin) state = _lookAhead(state);
+      if (state & stateNoBreak == 0) {
+        return breakAt;
+      }
+    }
+    state = moveBack(state, categoryEoT);
+    if (state >= stateLookaheadMin) state = _lookAhead(state);
+    if (state & stateNoBreak == 0) return cursor;
+    return -1;
+  }
+
+  int _lookAhead(int state) => lookAhead(base, start, cursor, state);
+}
+
+/// Request a lookahead for [state].
+///
+/// The [state] was output by the backwards grapheme cluster state
+/// machine and is above [stateLookaheadMin].
+/// The lookahead looks at the [base] string from just before [cursor]
+/// back to [start], to detect which actual state to enter.
+int lookAhead(String base, int start, int cursor, int state) {
+  assert(state >= stateLookaheadMin);
+  if (state == stateRegionalLookahead) {
+    return lookAheadRegional(base, start, cursor);
+  }
+  if (state == stateZWJPictographicLookahead) {
+    return lookAheadZWJPictorgraphic(base, start, cursor);
+  }
+  throw StateError("Unexpected state: ${state.toRadixString(16)}");
+}
+
+/// Counts preceding regional indicators.
+///
+/// The look-ahead for the backwards moving grapheme cluster
+/// state machine is called when two RIs are found in a row.
+/// The [cursor] points to the first code unit of the former of those RIs,
+/// and it preceding RIs until [start].
+/// If that count is even, there should not be a break before
+/// the second of the original RIs.
+/// If the count is odd, there should be a break, because that RI
+/// is combined with a prior RI in the string.
+int lookAheadRegional(String base, int start, int cursor) {
+  // Has just seen second regional indicator.
+  // Figure out if there are an odd or even number of preceding RIs.
+  // ALL REGIONAL INDICATORS ARE NON-BMP CHARACTERS.
+  int count = 0; // Include the current character triggering this.
+  int index = cursor;
+  while (index - 2 >= start) {
+    int tail = base.codeUnitAt(index - 1);
+    if (tail & 0xFC00 != 0xDC00) break;
+    int lead = base.codeUnitAt(index - 2);
+    if (lead & 0xFC00 != 0xD800) break;
+    int category = high(lead, tail);
+    if (category != categoryRegionalIndicator) break;
+    index -= 2;
+    count ^= 1;
+  }
+  if (count == 0) {
+    return stateRegionalEven | stateNoBreak;
+  } else {
+    return stateRegionalOdd;
+  }
+}
+
+/// Checks if a ZWJ+Pictographic token sequence should be broken.
+///
+/// The sequence should no be broken if the preceding code points
+/// are Pictographic Extend*.
+/// Only the [base] string from [start] to [cursor] is checked.
+/// The [cursor] points to the ZWJ code point.
+int lookAheadZWJPictorgraphic(String base, int start, int cursor) {
+  // Has just seen ZWJ+Pictographic. Check if preceeding is Pic Ext*.
+  // (If so, just move cursor back to the Pic).
+  int index = cursor;
+  while (index > start) {
+    int char = base.codeUnitAt(--index);
+    int prevChar = 0;
+    int category = categoryControl;
+    if (char & 0xFC00 != 0xDC00) {
+      category = low(char);
+    } else if (index > start &&
+        (prevChar = base.codeUnitAt(--index)) & 0xFC00 == 0xD800) {
+      category = high(prevChar, char);
+    } else {
+      break;
+    }
+    if (category == categoryPictographic) {
+      return stateZWJPictographic | stateNoBreak;
+    }
+    if (category != categoryExtend) break;
+  }
+  return stateExtend; // State for break before seeing ZWJ.
+}
+
+/// Whether there is a grapheme cluster boundary before [index] in [text].
+///
+/// This is a low-level function. There is no validation of the arguments.
+/// They should satisfy `0 <= start <= index <= end <= text.length`.
+bool isGraphemeClusterBoundary(String text, int start, int end, int index) {
+  assert(0 <= start);
+  assert(start <= index);
+  assert(index <= end);
+  assert(end <= text.length);
+  // Uses the backwards automaton because answering the question
+  // might be answered by looking only at the code points around the
+  // index, but it may also require looking further back. It never
+  // requires looking further ahead, though.
+  // The backwards automaton is built for this use case.
+  // Most of the apparent complication in this function is merely dealing with
+  // surrogates.
+  if (start < index && index < end) {
+    // Something on both sides of index.
+    int char = text.codeUnitAt(index);
+    int prevChar = text.codeUnitAt(index - 1);
+    int catAfter = categoryControl;
+    if (char & 0xF800 != 0xD800) {
+      catAfter = low(char);
+    } else if (char & 0xFC00 == 0xD800) {
+      // Lead surrogate. Combine with following tail surrogate,
+      // otherwise it's a control and always a boundary.
+      if (index + 1 >= end) return true;
+      int nextChar = text.codeUnitAt(index + 1);
+      if (nextChar & 0xFC00 != 0xDC00) return true;
+      catAfter = high(char, nextChar);
+    } else {
+      // Tail surrogate after index. Either combines with lead surrogate
+      // before or is always a bundary.
+      return prevChar & 0xFC00 != 0xD800;
+    }
+    int catBefore = categoryControl;
+    if (prevChar & 0xFC00 != 0xDC00) {
+      catBefore = low(prevChar);
+      index -= 1;
+    } else {
+      // If no prior lead surrogate, it's a control and always a boundary.
+      index -= 2;
+      if (start <= index) {
+        int prevPrevChar = text.codeUnitAt(index);
+        if (prevPrevChar & 0xFC00 != 0xD800) {
+          return true;
+        }
+        catBefore = high(prevPrevChar, prevChar);
+      } else {
+        return true;
+      }
+    }
+    var state = moveBack(stateEoTNoBreak, catAfter);
+    // It requires at least two moves from EoT to trigger a lookahead,
+    // either ZWJ+Pic or RI+RI.
+    assert(state < stateLookaheadMin);
+    state = moveBack(state, catBefore);
+    if (state >= stateLookaheadMin) {
+      state = lookAhead(text, start, index, state);
+    }
+    return state & stateNoBreak == 0;
+  }
+  // Always boundary at EoT or SoT, unless there is nothing between them.
+  return start != end;
 }
